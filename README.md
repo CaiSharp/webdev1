811299eff9bcdef48c213840cdb2fc8b63c35a88
Alexander Weber <alex.weber312@gmail.com>
2018-04-25 23:17:16 +0200

    added http, some more terminal logs

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # webdev1
 
-###basic visitor counter which uses a tcp client to server connection
+##basic visitor counter
 
-####how to use:
+##how to use:
 * run 'node server' to start the server
-* run 'node app' to make client requests to the server and get the visitor  count
+* lists both http and net server in terminal
+
+#####You can now either:
+
+* run 'node app' to make client requests to the server and get the visitor  count logged
+* visit localhost 127.0.0.1:80 and get the visitor count logged
+
+####or
+
+* visit localhost 127.0.0.1:3000 and view the counter on page / terminal has it logged as well
+
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const net = require('net');
+const http = require('http');
 const HOST = '127.0.0.1';
 const PORT = 80;
+const PORT2 = 3000;
 
 let visitorCount = 0;
 
-//create server with socket object
+//create server with socket object for client/server exchange
 net.createServer(function(socket) {
 
     //log client connection
@@ -15,7 +17,8 @@ net.createServer(function(socket) {
     socket.on('data', function(data) {
 
         //log message from client && give the visitorCount back
-        console.log('CLIENT MESSAGE' + socket.remoteAddress + ': ' + data);
+        console.log('CLIENT MESSAGE: ' + socket.remoteAddress + ': ' + data);
+        console.log('VISITOR COUNT: ' + visitorCount);
         socket.write("You're visitor " + visitorCount);
     });
 
@@ -26,4 +29,27 @@ net.createServer(function(socket) {
 
 }).listen(PORT, HOST);
 
-console.log('Server listening on ' + HOST +':'+ PORT);
+console.log('Server 1 listening on ' + HOST +':'+ PORT);
+
+
+//create server for http browser request
+http.createServer(function (req,res) {
+
+    //ignore favicon browser request
+    if (req.url === '/favicon.ico') {
+        res.writeHead(200, {'Content-Type': 'image/x-icon'});
+        res.end();
+        return;
+    }
+
+    //sets response head
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    //increases visitor count && logs it, displays it in the browser as well
+    visitorCount++;
+    res.write(`${visitorCount}`);
+    res.end();
+    console.log(`OI! You're visitor ${visitorCount}!`);
+}).listen(PORT2);
+
+console.log('Server 2 listening on ' + HOST +':'+ PORT2);
