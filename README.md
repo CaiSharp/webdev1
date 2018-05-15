77308f189e3fa82a6cab07d7a3b9f6a3279344ce
Alex <alex.weber312@gmail.com>
2018-05-16 00:08:22 +0200

    HA 5

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # webdev1
 
-###basic sort program
+###basic content delivery server
 
 run the server with either command: <br>
 
 * "*node server*" 
 * "*npm start*". 
 
-<br>Then use POSTMAN to POST on the following address: 
+<br>Then use POSTMAN to GET files on the following address: 
 
-**localhost:3000**
+**localhost:8000**
 
+The available files are:
+* "*kermit*" 
+* "*kermit.css*" 
+* "*kermit.html*" 
+* "*kermit.jpg*" 
+* "*kermit.png*" 
+* "*kermit.js*" 
 
+The **root folder** && **port** can be configured under **server.js** (const **directory,port**).<br> 
+However it is not recommended.
--- server.js
+++ server.js
@@ -1,75 +1,83 @@
 //GLOBAL VARIABLES
 const net = require('net');
-const port = 3000;
+const fs = require('fs');
+const path = require('path');
+
+const port = 8000;
+const directory = path.join(__dirname, 'public');
+
+const contentType = {
+    txt: 'text/plain',
+    html: 'text/html',
+    css: 'text/css',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    js: 'application/javascript'
+};
 
 //CREATE SERVER
-const server = net.createServer(function (socket) {
-    socket.on('data', function (data) {
-        let local;
-        let sortedArr;
+const server = net.createServer(socket => {
+    socket.on('data', data => {
+        let input = data.toString();
 
-        if(!data.toString().includes(`GET /favicon.ico HTTP/1.1`)){
+        //GET REQUEST && END SOCKET IF NOT VALID
+        let request = getRequest(input,socket);
 
-            //SPLIT IN LINES, STORE IN ARRAY
-            let lines = data.toString().split('\r\n');
+        //CREATE FILE PATH && SET CONTENT TYPE (PLAIN TEXT IN CASE THERE"S NONE)
+        let file = path.join(directory, request);
+        let type = contentType[path.extname(file).slice(1)] || 'text/plain';
 
-            //LOOK FOR LANGUAGE LINE IN ARRAY && RETURN
-            local = searchLine(lines,'Accept-Language:');
-            //CUT OUT LOCAL LANGUAGE
-            local = filterLang(local,0);
-
-            //FILTER BODY FROM HEAD && REMOVE \r\n
-            let indexEmptyLine = (lines.findIndex(el => el.length === 0));
-            let contentBody = lines.filter((el,index) => index > indexEmptyLine);
-            contentBody = contentBody.toString().split('\n');
-
-            //SORT WITH BUBBLESORT && compareLocal()
-            sortedArr = bubbleSort(contentBody,local);
-        }
-        //WRITE RESPONSE
-        sendResponse(socket,local,sortedArr);
+        //READ FILE FROM DIRECTORY && WRITE TO SOCKET
+        const fileSystem = fs.readFile(file,(error, data) => {
+            if (error) {
+                sendError(404,socket);
+            } else {
+                socket.write('HTTP/1.1 200 OK\n');
+                socket.write(`Content-Type: ${type}\n\n`);
+                socket.write(data);
+                socket.end();
+            }
+        });
+        console.log(file);
     });
 }).listen(port);
 
-function searchLine(array, string) {
-    let target;
+function getRequest(stringReq,socket) {
 
-    array.forEach( el => {
-        if(el.includes(string)){
-            target = el;
+    //SPLIT PATH FROM DATA AND RETURN IF VALID GET REQUEST
+    if(stringReq.match(/\n\r?\n\r?/)){
+        let lines = stringReq.split(/\n/)[0].split(' ');
+
+        if(lines[0] === 'GET'){
+            return lines[1];
+        } else {
+            sendError(501,socket);
         }
-    });
-    return target;
+    }
 }
 
-function filterLang(string,langCount) {
-    return string.replace('Accept-Language: ', '').split(',')[langCount];
-}
+function sendError(errorNum,socket) {
 
-function bubbleSort(arr,local) {
-    let swap;
+    switch (errorNum){
+        case 404:
+            socket.write('HTTP/1.1 404 Not Found\n');
+            socket.write('Content-Type: text/plain\n\n');
+            socket.write('Not Found');
+            socket.end();
+            break;
 
-    do {
-        swap = false;
-        for (let i = 1; i < arr.length; ++i) {
-            if (arr[i-1].localeCompare(arr[i],local) > 0) {
-                [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
-                swap = true;
-            }
-        }
-    } while (swap);
-    return arr;
-}
+        case 501:
+            socket.write('HTTP/1.1 501 Not Implemented\n');
+            socket.write('Content-Type: text/plain\n\n');
+            socket.write('Method not implemented');
+            socket.end();
+            break;
 
-function sendResponse(socket, local, arr) {
-    socket.write('HTTP/1.1 200 OK\r\n');
-    socket.write('Content-Type: text/plain; charset: utf-8\r\n');
-    socket.write(`\r\n`);
-    socket.write(`Language: ${local}`);
-    socket.write(`\r\n`);
-    arr.forEach(el => {
-        socket.write(`${el}`);
-        socket.write(`\r\n`);
-    });
-    socket.end();
+        default:
+            socket.write('HTTP/1.1 444 GG\n');
+            socket.write('Content-Type: text/plain\n\n');
+            socket.write('GG no RE, Something went wrong.');
+            socket.end();
+            break;
+    }
 }
